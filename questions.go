c7d1d5f4243ef9905fb9cc7d06de7787b1bda6ab
yykhomenko <yykhomenko@gmail.com>
2020-11-26 21:44:02 +0200

    added isQuestion logic

--- questions.go
+++ questions.go
@@ -9,24 +9,31 @@ var questions = map[string][]string{
 	"when":  {"когда"},
 	"why":   {"почему", "зачем"},
 	"how":   {"как"},
-	"do":    {"делать"},
-	"does":  {"делает"},
-	"did":   {"делал"},
-	"will":  {"буду"},
-	"am":    {"есть"},
-	"is":    {"есть"},
-	"are":   {"есть"},
-	"was":   {"был"},
-	"were":  {"были"},
 }
 
 func IsQuestion(s string) bool {
 	s = strings.ToLower(s)
+
 	for q := range questions {
 		if strings.HasPrefix(s, q) &&
 			strings.HasSuffix(s, "?") {
 			return true
 		}
 	}
+
+	for q := range do {
+		if strings.HasPrefix(s, q) &&
+			strings.HasSuffix(s, "?") {
+			return true
+		}
+	}
+
+	for q := range toBe {
+		if strings.HasPrefix(s, q) &&
+			strings.HasSuffix(s, "?") {
+			return true
+		}
+	}
+
 	return false
 }
