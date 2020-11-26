efc4eace1c4c37a70f124c1e6539a553d6d60a39
yykhomenko <yykhomenko@gmail.com>
2020-11-26 21:15:31 +0200

    added some pronouns

---
+++ pronouns.go
@@ -0,0 +1,28 @@
+package english
+
+var pronounsWho = map[string][]string{
+	"i":    {"я"},
+	"you":  {"вы"},
+	"he":   {"он"},
+	"she":  {"она"},
+	"we":   {"мы"},
+	"they": {"они"},
+}
+
+var pronounsWhom = map[string][]string{
+	"me":   {"меня"},
+	"you":  {"вас"},
+	"him":  {"его"},
+	"her":  {"ее"},
+	"us":   {"нас"},
+	"them": {"их"},
+}
+
+var pronounsWhose = map[string][]string{
+	"my":    {"мое"},
+	"your":  {"ваше"},
+	"his":   {"его"},
+	"her":   {"ее"},
+	"our":   {"наше"},
+	"their": {"их"},
+}
